d1102902693c914f3ac9cf2dcb5ff4b0f9a9eab8
Veera Sankar <sankarsivam6991@gmail.com>
2024-12-28 16:34:57 +0530

    signup and login functionality

--- netflix-clone/backend/controllers/auth.controller.js
+++ netflix-clone/backend/controllers/auth.controller.js
@@ -1,11 +1,125 @@
+import { User } from "../models/user.model.js";
+
+import bcryptjs from "bcryptjs";
+import { generateTokenAndCookie } from "../utils/generateToken.js";
+
 export async function signup(req, res) {
-  res.send("signup route");
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be atleast 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username });
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exists" });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username,
+      image,
+    });
+
+    generateTokenAndCookie(newUser._id, res);
+
+    await newUser.save();
+
+    // Remove password from the response
+    res.status(201).json({
+      success: true,
+      user: {
+        ...newUser._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    console.log("Error in Signup Controller", error.message);
+
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
 
 export async function login(req, res) {
-  res.send("login route");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    generateTokenAndCookie(user._id, res);
+
+    // Remove password from the response
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
 
 export async function logout(req, res) {
-  res.send("logout route");
+  try {
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Logged out succesfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
